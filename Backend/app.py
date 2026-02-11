13f2cfb8fe6a03bfac58354c23e832abbf06f723
BrownGinger12 <milesandray12@gmail.com>
2026-02-11 16:29:05 +0800

    final fix

--- Backend/app.py
+++ Backend/app.py
@@ -199,4 +199,4 @@ def delete_email(application_code):
     return delete_log(application_code)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
